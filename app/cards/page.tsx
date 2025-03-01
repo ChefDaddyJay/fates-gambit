625bce26e0541f17e2b893b83c8df162154f8f56
Joshua Tryon <joshuamtryon@gmail.com>
2025-02-28 23:55:58 -0600

    Lint fixes

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -2,19 +2,9 @@
 
 import { fetchCards } from "@/app/lib/data";
 import CardDisplay from "@/app/ui/cards/display";
-//import { useState } from "react";
 
 export default async function StorePage() {
     const cards = await fetchCards();
-
-    /*const [currentCard, setCard] = useState(cards[0]);
-
-    const pickCard = (index: number) => {
-        setCard(cards[index]);
-    }*/
-
-    let currentCard = cards[0];
-    const pickCard = (i: number) => {currentCard = cards[i]} 
     
     return (
         <CardDisplay cards={cards} />
