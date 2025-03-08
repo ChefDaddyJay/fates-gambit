cf432566f229fc586db3d1e43e8c5234379ee5d2
Joshua Tryon <joshuamtryon@gmail.com>
2025-03-07 22:41:32 -0600

    Fixed CardListSkeleton to avoid Suspense errors

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -8,7 +8,24 @@ export function CardListSkeleton() {
         placeholders.push(EmptyCard);
     }
     return (
-        <CardList cards={placeholders} currentCard={0} />
+        <div className="flex flex-wrap content-start
+            shadow-inner overflow-scroll bg-stone-950 rounded
+            p-auto w-full h-full">
+            {placeholders.map((card, i) => (
+                /* Single Card Layout */
+                <div className={`w-full h-10 mt-1 mx-2 p-1 rounded-md text-slate-50
+                    flex content-start justify-start bg-yellow-900
+                    hover:text-yellow-100 hover:shadow-xl hover:cursor-pointer`}
+                    key={i}>
+                    <div className={`w-1/4 mx-auto flex rounded border-2 border-black`} />
+                    <div className={`w-2/3 overflow-y
+                        flex flex-grow flex-nowrap justify-center items-center 
+                        text-center text-xs`}>
+                        <div>{card.name}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
 
